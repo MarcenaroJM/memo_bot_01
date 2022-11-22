65a36e7aa249d22a2427b7c604860507bc9010ad
Juan Martín Marcenaro <marcenarojuanmartin@gmail.com>
2022-11-21 21:51:16 -0300

    Will u work?

--- bot.py
+++ bot.py
@@ -5,32 +5,25 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 
+
 from request_open_weather import get_ow_forecast
 
 import os
 import random
 import redis
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 r = redis.from_url("redis://red-cdu0mopa6gdv3sp5q8bg:6379") # connection to the databse
 db_keys = r.keys(pattern='*')   # allows us to fetch data
-
 
 telegram_bot_token = "5658759506:AAEMEiLNPRLXKKX3Z0IZ9ZK1s1xuBGeqfqg"
 
 updater = Updater("5658759506:AAEMEiLNPRLXKKX3Z0IZ9ZK1s1xuBGeqfqg", use_context=True)
 
-j = updater.job_queue # Scheduled messages
-
 list_of_greets = ["GENIO", "FACHA", "MÁQUINA", "BEAR", "ANIMAL", "ÍDOLO", "OSO", "CRACK", "CAPO", "TITÁN"]
 
-def once(context: CallbackContext):
-    
-    message = "Hello, this message will be sent only once"
-    
-    # send message to all users
-    for keys in db_keys:
-        id = r.get(keys).decode("UTF-8")
-        context.bot.send_message(chat_id=id, text=message)
 
 def start(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id # Get user ID
@@ -44,6 +37,20 @@ def start(update: Update, context: CallbackContext):
 def weather(update: Update, context: CallbackContext):
 	update.message.reply_text(get_ow_forecast())
 
+
+def once(context: CallbackContext):
+    
+    message = "Hello, this message will be sent only once"
+    
+    # send message to all users
+    for keys in db_keys:
+        id = r.get(keys).decode("UTF-8")
+        context.bot.send_message(chat_id=id, text=message)
+
+j = updater.job_queue # Scheduled messages
+
+j.run_once(once, 30)
+
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('weather', weather))
 
@@ -55,5 +62,3 @@ updater.start_webhook(listen="0.0.0.0",
                       port=int(os.environ.get('PORT', 5000)),
                       url_path=telegram_bot_token,
                       webhook_url='https://good-morning-bot-01.onrender.com/' + telegram_bot_token)
-
-j.run_once(once, 30)
