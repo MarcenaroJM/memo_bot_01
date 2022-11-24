6a13fea75a28ec3a4b27e96dab3543073daad70b
Juan Martín Marcenaro <marcenarojuanmartin@gmail.com>
2022-11-23 23:36:39 -0300

    More changeeees

--- bot.py
+++ bot.py
@@ -30,11 +30,8 @@ def start(update: Update, context: CallbackContext):
     r.set(user_name, user_id)
     
     message = f"Hola {random.choice(list_of_greets)}, bienvenido al good-morning-bot!. Si te interesa, todos los días te puedo contar cómo va a estar el tiempo."
+    update.message.reply_text(message)
     
-    for keys in db_keys:
-        id = r.get(keys).decode("UTF-8")
-        context.bot.send_message(chat_id=id, text=message)
-
 def daily_message(context: CallbackContext):
 
     message = f"Buen día {random.choice(list_of_greets)}." + "\n" + get_ow_forecast()
@@ -75,7 +72,7 @@ j = updater.job_queue # Scheduled messages
 # j.run_daily(good_night, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=22, minute=22, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
 # j.run_daily(start, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=23, minute=12, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
 # j.run_daily(weather, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=23, minute=12, second=30, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
-j.run_daily(daily_message, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=23, minute=35, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
+j.run_daily(daily_message, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=7, minute=30, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 # updater.dispatcher.add_handler(CommandHandler('weather', weather))
