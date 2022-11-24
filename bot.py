2edac15e308645de78ac7136e4c8c937f5b6d666
Juan Martín Marcenaro <marcenarojuanmartin@gmail.com>
2022-11-23 23:14:07 -0300

    More changeeees

--- bot.py
+++ bot.py
@@ -23,8 +23,8 @@ updater = Updater("5658759506:AAEMEiLNPRLXKKX3Z0IZ9ZK1s1xuBGeqfqg", use_context=
 list_of_greets = ["GENIO", "FACHA", "MÁQUINA", "BEAR", "ANIMAL", "ÍDOLO", "OSO", "CRACK", "CAPO", "TITÁN"]
 
 
-# def start(update: Update, context: CallbackContext):
-def start(context: CallbackContext):
+def start(update: Update, context: CallbackContext):
+    
     user_id = update.message.from_user.id # Get user ID
     user_name = update.message.from_user.name # Get USERNAME
     r.set(user_name, user_id)
@@ -34,40 +34,51 @@ def start(context: CallbackContext):
     for keys in db_keys:
         id = r.get(keys).decode("UTF-8")
         context.bot.send_message(chat_id=id, text=message)
+
+def daily_message(context: CallbackContext):
+
+    message = f"Buen día {random.choice(list_of_greets)}. " + get_ow_forecast()
+    # send message to all users
+    for keys in db_keys:
+        id = r.get(keys).decode("UTF-8")
+        context.bot.send_message(chat_id=id, text=message)
+
 	
 # def weather(update: Update, context: CallbackContext):
-def weather(context: CallbackContext):
-	#update.message.reply_text(get_ow_forecast())
-    message = get_ow_forecast()
-    
-    for keys in db_keys:
-        id = r.get(keys).decode("UTF-8")
-        context.bot.send_message(chat_id=id, text=message)
+# def weather(context: CallbackContext):
 
-def once(context: CallbackContext):
+#     message = get_ow_forecast()
     
-    message = "Hello, this message will be sent only once"
-    
-    # send message to all users
-    for keys in db_keys:
-        id = r.get(keys).decode("UTF-8")
-        context.bot.send_message(chat_id=id, text=message)
+#     for keys in db_keys:
+#         id = r.get(keys).decode("UTF-8")
+#         context.bot.send_message(chat_id=id, text=message)
 
-def good_night(context: CallbackContext):
+# def once(context: CallbackContext):
     
-    message = "Chau bro, que descanses"
-    # send message to all users
-    for keys in db_keys:
-        id = r.get(keys).decode("UTF-8")
-        context.bot.send_message(chat_id=id, text=message)
+#     message = "Hello, this message will be sent only once"
+    
+#     # send message to all users
+#     for keys in db_keys:
+#         id = r.get(keys).decode("UTF-8")
+#         context.bot.send_message(chat_id=id, text=message)
+
+# def good_night(context: CallbackContext):
+    
+#     message = "Chau bro, que descanses"
+#     # send message to all users
+#     for keys in db_keys:
+#         id = r.get(keys).decode("UTF-8")
+#         context.bot.send_message(chat_id=id, text=message)
 
 j = updater.job_queue # Scheduled messages
+
 # j.run_daily(good_night, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=22, minute=22, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
-j.run_daily(start, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=23, minute=12, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
-j.run_daily(weather, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=23, minute=12, second=30, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
+# j.run_daily(start, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=23, minute=12, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
+# j.run_daily(weather, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=23, minute=12, second=30, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
+j.run_daily(daily_message, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=23, minute=20, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('weather', weather))
+# updater.dispatcher.add_handler(CommandHandler('weather', weather))
 
 # If the application runs locally:
 #updater.start_polling()
