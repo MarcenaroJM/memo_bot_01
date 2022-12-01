247a208dbc2ad9dc90a033da4703eec6cbc9e2ef
Juan Martín Marcenaro <marcenarojuanmartin@gmail.com>
2022-12-01 18:43:06 -0300

    Let's make it wooork

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ import redis
 import datetime
 import pytz
 
-r = redis.from_url("redis://red-cdu0mopa6gdv3sp5q8bg:6379") # connection to the databse
+r = redis.from_url("redis://red-cdu0mopa6gdv3sp5q8bg:6379") # connection to the database
 db_keys = r.keys(pattern='*')   # allows us to fetch data
 
 telegram_bot_token = "5658759506:AAEMEiLNPRLXKKX3Z0IZ9ZK1s1xuBGeqfqg"
@@ -23,32 +23,32 @@ updater = Updater("5658759506:AAEMEiLNPRLXKKX3Z0IZ9ZK1s1xuBGeqfqg", use_context=
 list_of_greets = ["GENIO", "FACHA", "MÁQUINA", "BEAR", "ANIMAL", "ÍDOLO", "OSO", "CRACK", "CAPO", "TITÁN"]
 
 
-def start(update: Update, context: CallbackContext):
+# def start(update: Update, context: CallbackContext):
     
-    user_id = update.message.from_user.id # Get user ID
-    user_name = update.message.from_user.name # Get USERNAME
-    r.set(user_name, user_id)
+#     user_id = update.message.from_user.id # Get user ID
+#     user_name = update.message.from_user.name # Get USERNAME
+#     r.set(user_name, user_id)
     
-    message = f"Hola {random.choice(list_of_greets)}, bienvenido al good_morning_bot!. Si te interesa, todos los días te puedo contar cómo va a estar el tiempo. Saludis"
-    update.message.reply_text(message)
+#     message = f"Hola {random.choice(list_of_greets)}, bienvenido al good_morning_bot!. Si te interesa, todos los días te puedo contar cómo va a estar el tiempo. Saludis"
+#     update.message.reply_text(message)
     
-def daily_message(context: CallbackContext):
+# def daily_message(context: CallbackContext):
 
-    message = f"Buen día {random.choice(list_of_greets)}." + "\n" + get_ow_forecast()
-    # send message to all users
-    for keys in db_keys:
-        id = r.get(keys).decode("UTF-8")
-        context.bot.send_message(chat_id=id, text=message)
-
-	
-# def weather(update: Update, context: CallbackContext):
-# def weather(context: CallbackContext):
-
-#     message = get_ow_forecast()
-    
+#     message = f"Buen día {random.choice(list_of_greets)}." + "\n" + get_ow_forecast()
+#     # send message to all users
 #     for keys in db_keys:
 #         id = r.get(keys).decode("UTF-8")
 #         context.bot.send_message(chat_id=id, text=message)
+
+	
+# def weather(update: Update, context: CallbackContext):
+def weather(context: CallbackContext):
+
+    message = get_ow_forecast()
+    
+    for keys in db_keys:
+        id = r.get(keys).decode("UTF-8")
+        context.bot.send_message(chat_id=id, text=message)
 
 # def once(context: CallbackContext):
     
@@ -67,15 +67,15 @@ def daily_message(context: CallbackContext):
 #         id = r.get(keys).decode("UTF-8")
 #         context.bot.send_message(chat_id=id, text=message)
 
-j = updater.job_queue # Scheduled messages
+# j = updater.job_queue # Scheduled messages
 
 # j.run_daily(good_night, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=22, minute=22, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
 # j.run_daily(start, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=23, minute=12, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
 # j.run_daily(weather, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=23, minute=12, second=30, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
-j.run_daily(daily_message, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=8, minute=0, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
+# j.run_daily(daily_message, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.time(hour=8, minute=0, second=00, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires")))
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-# updater.dispatcher.add_handler(CommandHandler('weather', weather))
+# updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('weather', weather))
 
 # If the application runs locally:
 #updater.start_polling()
